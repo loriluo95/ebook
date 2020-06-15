a4829e5e45cc2f6db0e626c8b2334c212cc730c2
lori <zluo2@wpi.edu>
2020-06-15 16:06:05 -0700

    set up mysql connection

---
+++ src/main/java/main/java/com/ebookstore/EbookApplication.java
@@ -0,0 +1,11 @@
+package main.java.com.ebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EbookApplication.class, args);
+    }
+}
