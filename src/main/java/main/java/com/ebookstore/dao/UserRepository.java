59e75ba007b71c7a46643a184bec7709c7c6e7f2
lori <zluo2@wpi.edu>
2020-06-16 11:29:42 -0700

    display user table

--- src/main/java/main/java/com/ebookstore/dao/UserRepository.java
+++ src/main/java/main/java/com/ebookstore/dao/UserRepository.java
@@ -1,5 +1,6 @@
 package main.java.com.ebookstore.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import main.java.com.ebookstore.model.User;
@@ -7,6 +8,6 @@ import main.java.com.ebookstore.model.User;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
