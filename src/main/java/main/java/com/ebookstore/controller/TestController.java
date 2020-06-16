2cf8060c42ac6fd5eb55c413a48fd365a5078bcd
lori <zluo2@wpi.edu>
2020-06-16 10:17:16 -0700

    delete unused files

--- src/main/java/main/java/com/ebookstore/controller/TestController.java
+++ src/main/java/main/java/com/ebookstore/controller/TestController.java
@@ -8,19 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class TestController {
 
-//    @GetMapping("/greeting")
-//    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "greeting";
-//    }
-//    @GetMapping({"/"})
-//    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
-//        model.addAttribute("name", name);
-//        return "hello";
-//    }
-    @GetMapping({"/"})
-    public String home(Model model) {
-        return "home";
+    @GetMapping({"/", "/hello"})
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
     }
-
 }
