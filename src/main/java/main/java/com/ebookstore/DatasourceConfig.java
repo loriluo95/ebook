fa3275ad3a10b32697572e2f52e64eb3ec00b9a4
lori <zluo2@wpi.edu>
2020-06-18 18:24:44 -0700

    start react app

--- src/main/java/main/java/com/ebookstore/DatasourceConfig.java
+++ src/main/java/main/java/com/ebookstore/DatasourceConfig.java
@@ -10,17 +10,17 @@ import javax.sql.DataSource;
 public class DatasourceConfig {
     @Bean
     public DataSource datasource() {
-//        return DataSourceBuilder.create()
-//                .driverClassName("com.mysql.cj.jdbc.Driver")
-//                .url("jdbc:mysql://localhost:3306/EBOOK?serverTimezone=UTC")
-//                .username("root")
-//                .password("pass")
-//                .build();
         return DataSourceBuilder.create()
-                .driverClassName("com.mysql.jdbc.Driver")
-                .url("jdbc:mysql://b0008f21574269:40d3e5b7@us-cdbr-east-05.cleardb.net/heroku_7733fabc380c367?reconnect=true")
-                .username("b0008f21574269")
-                .password("40d3e5b7")
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:mysql://localhost:3306/EBOOK?serverTimezone=UTC")
+                .username("root")
+                .password("pass")
                 .build();
+//        return DataSourceBuilder.create()
+//                .driverClassName("com.mysql.jdbc.Driver")
+//                .url("jdbc:mysql://b0008f21574269:40d3e5b7@us-cdbr-east-05.cleardb.net/heroku_7733fabc380c367?reconnect=true")
+//                .username("b0008f21574269")
+//                .password("40d3e5b7")
+//                .build();
     }
 }
